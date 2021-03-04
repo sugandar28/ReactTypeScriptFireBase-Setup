45b77f2d98c4da87f7468d45aff4bef50b050b34
Sugandar <rsugandhar1998@gmail.com>
2021-03-04 10:18:24 +0530

    firebase config added

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,13 +3,16 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { Routes } from '../Routes'
 import store from '../Redux/Store';
-
+import { Firebase, FirebaseContext } from '../Firebase';
 function App() {
+  const firebase = new Firebase();
   return (
     <Provider store={store}>
+      <FirebaseContext.Provider value={firebase}>
         <Router>
           <Routes />
         </Router>
+      </FirebaseContext.Provider>
     </Provider>
   );
 }
